81d8109dc2ba4881d78585d00aed39c48b788025
Samara hespanhol <samarahespanhol@gmail.com>
2026-06-22 23:16:13 -0400

    alteração pequena na tela equipe dto

--- Dtos/EquipeDto.cs
+++ Dtos/EquipeDto.cs
@@ -4,7 +4,7 @@ namespace WashApi.Dtos
 {
     public class EquipeDto
     {
-        [Required(ErrorMessage = "O nome é obrigatório")]
+        [Required(ErrorMessage = "O nome é obrigatório!")]
         public required string Nome { get; set; }
     }
 }
