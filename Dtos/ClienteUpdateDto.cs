76c796d88c458359311f386412ce8606287040f4
Samara hespanhol <samarahespanhol@gmail.com>
2026-06-22 23:15:18 -0400

    alteração no cliente update dto

--- Dtos/ClienteUpdateDto.cs
+++ Dtos/ClienteUpdateDto.cs
@@ -4,14 +4,14 @@ namespace WashApi.Dtos
 {
     public class ClienteUpdateDto
     {
-        [Required(ErrorMessage = "O nome é obrigatório")]
+        [Required(ErrorMessage = "O nome é obrigatório!")]
         public required string Nome { get; set; }
 
-        [Required(ErrorMessage = "O e-mail é obrigatório")]
-        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        [Required(ErrorMessage = "O e-mail é obrigatório!")]
+        [EmailAddress(ErrorMessage = "E-mail inválido!")]
         public required string Email { get; set; }
 
-        [Required(ErrorMessage = "O telefone é obrigatório")]
+        [Required(ErrorMessage = "O telefone é obrigatório!")]
         public required string Telefone { get; set; }
     }
 }
