e9240dcf6583ef6d83ef69b426de7b119769edf6
Samara hespanhol <samarahespanhol@gmail.com>
2026-06-22 23:18:59 -0400

    pequena alteracao na tela FuncionarioupdateDto

--- Dtos/FuncionarioUpdateDto.cs
+++ Dtos/FuncionarioUpdateDto.cs
@@ -4,10 +4,10 @@ namespace WashApi.Dtos
 {
     public class FuncionarioUpdateDto
     {
-        [Required(ErrorMessage = "O nome é obrigatório")]
+        [Required(ErrorMessage = "O nome é obrigatório!")]
         public required string Nome { get; set; }
 
-        [Required(ErrorMessage = "O cargo é obrigatório")]
+        [Required(ErrorMessage = "O cargo é obrigatório!")]
         public required string Cargo { get; set; }
     }
 }
